ad774fa19de40512acd0f32b16e52e969fe7a1c8
Gradi Ellis <[ellis.gradi@gmail.com]>
2019-08-28 14:21:28 -0500

    new functions added

---
+++ src/scripts/journal.js
@@ -0,0 +1,18 @@
+const journalEntry = [
+    {
+        entryDate: "08/13/2019",
+        conceptsTitle: "html",
+        longForm: "Today we learned the very basics of html",
+        mood: ""
+    },
+    {
+        entryDate: "",
+        conceptsTitle: "",
+        longForm: "",
+        mood: "pumped"
+    }
+];
+
+
+
+console.log(journalEntry);
