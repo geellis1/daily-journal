4124d20c9b9f95f0c1b3c91e4a9913fd2165eee2
Gradi Ellis <[ellis.gradi@gmail.com]>
2019-09-01 22:22:12 -0500

    added js-part2

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,18 +1,33 @@
-const journalEntry = [
+
+const journalEntries = [
     {
         entryDate: "08/13/2019",
         conceptsTitle: "html",
         longForm: "Today we learned the very basics of html",
-        mood: ""
+        mood: "pumped",
     },
     {
         entryDate: "",
         conceptsTitle: "",
         longForm: "",
-        mood: "pumped"
+        mood: "",
     }
 ];
 
+console.log(journalEntries);
 
 
-console.log(journalEntry);
+const makeJournalEntryComponent = (item) => {
+    return `<div>
+    <title>${item.conceptsTitle}</title>
+    <p>${item.entryDate}</p>
+    <p>${item.longForm}</p>
+    <p>${item.mood}</p>
+</div>`   
+}
+
+const entryInfo = document.querySelector("#container")
+
+for (let i = 0; i < journalEntries.length; i++) {
+    entryInfo.innerHTML += makeJournalEntryComponent(journalEntries[i])
+    };
