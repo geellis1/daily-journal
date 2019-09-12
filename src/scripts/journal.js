e2b58cea2720ad0fe1876923e3915da34becac29
Gradi Ellis <[ellis.gradi@gmail.com]>
2019-09-12 16:02:23 -0500

    working alert for input

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,8 +1,59 @@
-import myObject from "./entriesDOM.js"
 import API from "./data.js"
 import renderJournalEntries from "./entriesDOM.js"
 
 API.getJournalEntries().then(renderJournalEntries)
+
+
+// Event listener for the 'create new journal entry' button
+document.querySelector(".submit").addEventListener("click", () => {
+
+    // 1. needs to get values of the inputs/dropdowns
+// they're stored in variables to use later
+    const journalDate = document.querySelector("#journalDate").value;
+    const conceptsCovered = document.querySelector("#concepts").value;
+    const entryNotes = document.querySelector("#entryID").value;
+    // const journalMood = document.querySelector("#mood").value;
+    
+  if (journalDate === "" || conceptsCovered === "" || entryNotes === "" || journalMood === "")
+      alert("All fields are required");
+}
+
+)
+
+console.log(journalDate);
+
+//     // IT'S LATER
+//     // 2. needs to build an entry object
+//     const newEntryObj = createNewEntry(name, type, flavor, glaze, toppings)
+//     console.log("my new donut pls", newDonutObj)
+
+//     // 3. maybe clear inputs?
+//     document.querySelector("#name-input").value = "";
+
+//     // 4. clear donut-container before adding new donut
+//     document.querySelector("#donutResults").innerHTML = "";
+
+//     // 5. I need to save donut to the json
+//     API.createDonut(newDonutObj).then(() => {
+
+
+//         // 6. get all the donuts again
+//         API.getDonuts().then((allDonuts) => {
+//             allDonuts.forEach(donut => {
+//                 // 7. needs to send donut to DOM
+//                 addDonutToDOM(donut)
+//             })
+//         })
+
+//     })
+// })
+
+
+
+
+
+
+
 
 
 
